c39be6813313816e68392eabde002cf05bd07243
hina-naeem <hinaeem@gmail.com>
2023-08-20 17:32:58 +0500

    Class Problems Solved

--- class05/index.ts
+++ class05/index.ts
@@ -8,3 +8,33 @@ for(let i=0; i<100;i++){
     }
 }
 console.log("Result: ", sum,list);
+
+
+// Create a function that takes an array of numbers as parameter. use a while loop to calculate and return sum of all the numbers in the array
+
+
+function calculateSum(list: number[]): number {
+    let sum:number = 0;
+    let i:number = 0;
+    while(i<list.length) {
+        sum+=list[i];
+        i++;
+    }
+    return sum;
+}
+
+console.log("sum of array [1,2,3,4,5,6,7,8,9,10]:",calculateSum([1,2,3,4,5,6,7,8,9,10]));
+
+
+// Create a function that takes a positive integer as a parameter and uses a while loop to calculate and display the factorial of that number
+
+function calculateFactorial(num:number):number {
+    let factorial = 1;
+    while(num >=1){
+        factorial *= num;
+        num--;
+    }
+    return factorial;
+}
+
+console.log("10! = ",calculateFactorial(5));
